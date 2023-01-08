55506f0b7e783160340f74ce1434bf7cf7775991
Andres Vega <andresvegabotero@gmail.com>
2023-01-08 13:18:52 +0100

    restruckturing the code

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
-/*********
-  Rui Santos
-  Complete project details at http://randomnerdtutorials.com  
-*********/
+//Include
 
-#include <FS.h> //this needs to be first, or it all crashes and burns...
+#include <FS.h> 
 #include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>          // https://github.com/tzapu/WiFiManager
 #include <ArduinoJson.h>          // https://github.com/bblanchon/ArduinoJson
+
+#include "WiFiSetup.h"
 
 // Set web server port number to 80
 WiFiServer server(80);
@@ -23,122 +23,19 @@ String outputState = "off";
 char output[2] = "5";
 char mqtt_server[40];
 
-//flag for saving data
-bool shouldSaveConfig = false;
-
-//callback notifying us of the need to save config
-void saveConfigCallback () {
-  Serial.println("Should save config");
-  shouldSaveConfig = true;
-}
-
 void setup() {
-  Serial.begin(115200);
-  
-  //clean FS, for testing
-  //SPIFFS.format();
+    //Serial Start
+    Serial.begin(115200);
 
-  //read configuration from FS json
-  Serial.println("mounting FS...");
+    // Set Up Wifi Connection of needed (and variable storage)
+    setUpWiFi();
 
-  if (SPIFFS.begin()) {
-    Serial.println("mounted file system");
-    if (SPIFFS.exists("/config.json")) {
-      //file exists, reading and loading
-      Serial.println("reading config file");
-      File configFile = SPIFFS.open("/config.json", "r");
-      if (configFile) {
-        Serial.println("opened config file");
-        size_t size = configFile.size();
-        // Allocate a buffer to store contents of the file.
-        std::unique_ptr<char[]> buf(new char[size]);
-
-        configFile.readBytes(buf.get(), size);
-        DynamicJsonBuffer jsonBuffer;
-        JsonObject& json = jsonBuffer.parseObject(buf.get());
-        json.printTo(Serial);
-        if (json.success()) {
-          Serial.println("\nparsed json");
-          strcpy(output, json["output"]);
-          strcpy(mqtt_server, json["mqtt_server"]);
-        } else {
-          Serial.println("failed to load json config");
-        }
-      }
-    }
-  } else {
-    Serial.println("failed to mount FS");
-  }
-  //end read
-  
-  WiFiManagerParameter custom_output("output", "output", output, 2);
-  WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
-
-  // WiFiManager
-  // Local intialization. Once its business is done, there is no need to keep it around
-  WiFiManager wifiManager;
-
-  //set config save notify callback
-  wifiManager.setSaveConfigCallback(saveConfigCallback);
-  
-  // set custom ip for portal
-  //wifiManager.setAPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
-
-  //add all your parameters here
-  wifiManager.addParameter(&custom_output);
-  wifiManager.addParameter(&custom_mqtt_server);
-
-  // Uncomment and run it once, if you want to erase all the stored information
-  wifiManager.resetSettings();
-
-  //set minimu quality of signal so it ignores AP's under that quality
-  //defaults to 8%
-  //wifiManager.setMinimumSignalQuality();
-  
-  //sets timeout until configuration portal gets turned off
-  //useful to make it all retry or go to sleep
-  //in seconds
-  //wifiManager.setTimeout(120);
-
-  // fetches ssid and pass from eeprom and tries to connect
-  // if it does not connect it starts an access point with the specified name
-  // here  "AutoConnectAP"
-  // and goes into a blocking loop awaiting configuration
-  wifiManager.autoConnect("AutoConnectAP");
-  // or use this for auto generated name ESP + ChipID
-  //wifiManager.autoConnect();
-  
-  // if you get here you have connected to the WiFi
-  Serial.println("Connected.");
-  strcpy(mqtt_server, custom_mqtt_server.getValue());
-  
-  strcpy(output, custom_output.getValue());
-
-  //save the custom parameters to FS
-  if (shouldSaveConfig) {
-    Serial.println("saving config");
-    DynamicJsonBuffer jsonBuffer;
-    JsonObject& json = jsonBuffer.createObject();
-    json["output"] = output;
-    json["mqtt_server"] = mqtt_server;
-
-    File configFile = SPIFFS.open("/config.json", "w");
-    if (!configFile) {
-      Serial.println("failed to open config file for writing");
-    }
-
-    json.printTo(Serial);
-    json.printTo(configFile);
-    configFile.close();
-    //end save
-  }
-
-  // Initialize the output variables as outputs
-  pinMode(atoi(output), OUTPUT);
-  // Set outputs to LOW
-  digitalWrite(atoi(output), LOW);;
-  
-  server.begin();
+    // Initialize the output variables as outputs
+    pinMode(atoi(output), OUTPUT);
+    // Set outputs to LOW
+    digitalWrite(atoi(output), LOW);;
+    
+    server.begin();
 }
 
 void loop(){
