6036f40f61effe5e5be671e1dd2c40050054fe05
Andrés <andresvegabotero@gmail.com>
2023-04-27 19:19:35 +0200

    Cleanup

--- ARCHIV/WiFiSetup.h
+++
@@ -1,9 +0,0 @@
-#ifndef WIFISETUP_H_INCLUDED
-#define WIFISETUP_H_INCLUDED
-/* ^^ these are the include guards */
-
-/* Prototypes for the functions */
-/* Setup Wifi */
-void setUpWiFi(void);
-
-#endif
